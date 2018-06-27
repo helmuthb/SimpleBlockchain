b6a89655e86fd569eba66544bbb7d4f26596a021
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2018-06-28 00:36:35 +0200

    Added leveldb support

--- simpleChain.js
+++ simpleChain.js
@@ -1,4 +1,5 @@
 const SHA256 = require('crypto-js/sha256');
+const level = require('level');
 
 /**
  * Block Class
@@ -18,7 +19,8 @@ class Block {
   }
 
   calculateHash() {
-    return SHA256(JSON.stringify(this)).toString();
+    const hash = SHA256(JSON.stringify(this)).toString();
+    return hash;
   }
 
   validateHash() {
@@ -36,75 +38,278 @@ class Block {
 }
 
 /**
+ * Queue operator
+ * A mechanism to have a linked list of operations waiting to be performed.
+ */
+class QueueOps {
+  constructor() {
+    this.first = null;
+    this.last = null;
+  }
+
+  _doOp(fn, resolve, reject) {
+    let result = fn();
+    if ('then' in result) {
+      result.then(data => {
+        resolve(data);
+        this._runNext();
+      }, err => {
+        reject(err);
+        this._runNext();
+      })
+    }
+    else {
+      resolve(result);
+      this._runNext();
+    }
+  }
+
+  /**
+   * Put the function fn to the list.
+   * The function fn returns a promise, and the function add returns an
+   * equivalent promise.
+   */
+  add(fn) {
+    // are we first?
+    if (!this.last) {
+      this.first = {
+        'next': null,
+        'operation': null
+      };
+      this.last = this.first;
+      return new Promise((resolve, reject) => {
+        this._doOp(fn, resolve, reject);
+      });
+    }
+    else {
+      // no, we add us to the end of the list
+      return new Promise((resolve, reject) => {
+        this.last.next = {
+          'next': null,
+          'operation': fn,
+          'resolve': resolve,
+          'reject': reject
+        };
+        this.last = this.last.next;
+      });
+    }
+  }
+
+  /**
+   * Internal function: run the next from the operations queue
+   */
+  _runNext() {
+    while (this.first !== null && this.first.operation === null) {
+      this.first = this.first.next;
+    }
+    if (this.first === null) {
+      // we have worked through the queue
+      this.last = null;
+      return;
+    }
+    let operation = this.first.operation;
+    let resolve = this.first.resolve;
+    let reject = this.first.reject;
+    this.first = this.first.next;
+    if (this.first == null) {
+      this.last = null;
+    }
+    this._doOp(operation, resolve, reject);
+  }
+}
+
+/**
  * Blockchain Class
  * Class with a constructor for new blockchain
  */
 class Blockchain {
   constructor() {
     this.chain = [];
-    this.addBlock(new Block('First block in the chain - Genesis block'));
+    this.queue = new QueueOps();
+    this.doNext(() => {
+      return new Promise((resolve, reject) => {
+        this.chainDB = level('./chaindb', {}, (err, db) => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            this._getBlockHeight().then(height => {
+              if (height < 0) {
+                this._addBlock(new Block('First block in the chain - Genesis block')).then(resolve, reject);
+              }
+              else {
+                resolve(true);
+              }
+            })
+          }
+        });
+      });
+    });
   }
 
-  // Add new block
-  addBlock(aBlock) {
+  /**
+   * runNext - queue an operation to be run
+   */
+  doNext(fn) {
+    return this.queue.add(fn);
+  }
+
+  reset() {
+    return this.chainDB.del("height");
+  }
+
+  /**
+   * Add a block
+   */
+  _addBlock(aBlock) {
     const newBlock = aBlock.clone();
-    const height = this.getBlockHeight();
-    newBlock.height = height + 1;
-    newBlock.time = new Date().getTime().toString().slice(0, -3);
-    if (height >= 0) {
-      const previousBlock = this.chain[height];
-      newBlock.previousBlockHash = previousBlock.hash;
-    }
-    newBlock.hash = newBlock.calculateHash();
-    this.chain[height + 1] = newBlock;
+    return this._getBlockHeight().then(height => {
+      newBlock.height = 1 + Number(height);
+      newBlock.time = new Date().getTime().toString().slice(0, -3);
+      let p;
+      if (height >= 0) {
+        p = this._getBlock(height).then(block => {
+          newBlock.previousBlockHash = block.hash;
+          return newBlock;
+        });
+      }
+      else {
+        p = Promise.resolve(newBlock);
+      }
+      return p.then(nb => {
+        nb.hash = nb.calculateHash();
+        return this.chainDB.put(height+1, JSON.stringify(nb)).then(() => {
+          return this.chainDB.put("height", height+1);
+        });
+      });
+    });
+  }
+
+  addBlock(aBlock) {
+    return this.doNext(() => {
+      return this._addBlock(aBlock);
+    })
+  }
+
+  updateBlock(height, block) {
+    return this.doNext(() => {
+      return this.chainDB.put(height, JSON.stringify(block));
+    })
+  }
+
+  // output the content of the chainDB
+  outputChain() {
+    return this.doNext(() => {
+      return new Promise((resolve, reject) => {
+        this.chainDB.createReadStream()
+          .on('data', data => console.log(data.key, '=', data.value))
+          .on('error', err => console.log("Oh! ", er))
+          .on('close', () => console.log("Stream closed"))
+          .on('end', () => resolve());
+      });
+    });
   }
 
   // Get block height promise
+  _getBlockHeight() {
+    return this.chainDB.get("height").then(height => {
+      return Number(height);
+    }, err => {
+      return -1;
+    });
+  }
+
   getBlockHeight() {
-    return this.chain.length - 1;
+    return this.doNext(() => {
+      return this._getBlockHeight();
+    })
   }
 
   // get block
+  _getBlock(blockHeight) {
+    return this.chainDB.get(blockHeight).then(block => {
+      block = JSON.parse(block);
+      block.__proto__ = Block.prototype;
+      return block;
+    });
+  }
+
   getBlock(blockHeight) {
-    return this.chain[blockHeight];
+    return this.doNext(() => {
+      return this._getBlock(blockHeight);
+    });
   }
 
   // push block
   pushBlock(block) {
-    this.chain.push(block);
+    return this.doNext(() => {
+      return this.getBlockHeight.then(height => {
+        return this.chainDB.put(height+1, block);
+      })
+    })
   }
 
   // validate block
   validateBlock(blockHeight) {
     // get block object
-    const block = this.getBlock(blockHeight);
-    return block.validateHash();
+    return this.doNext(() => {
+      return this._getBlock(blockHeight).then(block => {
+        return block.validateHash();
+      })
+    });
+  }
+
+  // get all blocks (for validation)
+  _getAllBlocks() {
+    const blocksP = [];
+    return this._getBlockHeight().then(height => {
+      for (let i=0; i<=height; i++) {
+        blocksP[i] = this._getBlock(i);
+      }
+      return Promise.all(blocksP);
+    });
   }
 
   // Validate blockchain
   validateChain() {
-    const errorLog = [];
-    for (let i = 0; i < this.getBlockHeight(); i++) {
-      const block = this.getBlock(i);
-      const nextBlock = this.getBlock(i + 1);
-      // validate block
-      if (!block.validateHash()) {
-        errorLog.push(i);
-      }
-      // compare blocks hash link
-      const blockHash = block.hash;
-      const previousHash = nextBlock.previousBlockHash;
-      if (blockHash !== previousHash) {
-        errorLog.push(i);
-      }
-    }
-    if (errorLog.length > 0) {
-      console.log(`Block errors = ${errorLog.length}`);
-      console.log(`Blocks: ${errorLog}`);
-      return false;
-    }
-    console.log('No errors detected');
-    return true;
+    return this.doNext(() => {
+      return this._getAllBlocks().then(blocks => {
+        const errorLog = [];
+        const height = blocks.length-1;
+        for (let i=0; i<=height; i++) {
+          if(!blocks[i].validateHash()) {
+            errorLog.push(i);
+          }
+          if (i < height) {
+            if (blocks[i].hash != blocks[i+1].previousBlockHash) {
+              errorLog.push(i);
+            }
+          }
+        }
+        if (errorLog.length > 0) {
+          console.log(`Block errors = ${errorLog.length}`);
+          console.log(`Blocks: ${errorLog}`);
+          return false;
+        }
+        console.log('No errors detected');
+        return true;
+      });
+    });
+  }
+  
+  close() {
+    return this.doNext(() => {
+      return new Promise((resolve, reject) => {
+        this.chainDB.close(err => {
+          if (err) {
+            reject(err);
+          }
+          else {
+            resolve();
+          }
+        });
+      });
+    });
   }
 }
 
--- spec/simpleChainSpec.js
+++ spec/simpleChainSpec.js
@@ -1,27 +1,50 @@
-describe("SimpleChain", function() {
-  var {Block, Blockchain} = require('../simpleChain');
-  
-    beforeEach(function() {
-      blockchain = new Blockchain();
-    });
-    
-    it("should validate a correct Blockchain", function() {
-      for (let i=0; i<10; i++) {
-        blockchain.addBlock(new Block("test data " + i));
-      }
-      expect(blockchain.validateChain()).toBe(true);
-    });
+describe('SimpleChain', () => {
+  const { Block, Blockchain } = require('../simpleChain');
 
-    it("should raise errors in manipulated chain", function() {
-      for (let i=0; i<10; i++) {
-        blockchain.addBlock(new Block("test data " + i));
+  beforeEach((done) => {
+    blockchain = new Blockchain();
+    blockchain.doNext(() => {
+      let promise = Promise.resolve(true);
+      for (let i = 0; i < 10; i++) {
+        promise = promise.then(() => {
+          return blockchain._addBlock(new Block(`test data ${i}`));
+        });
       }
-      let inducedErrorBlocks = [2,4,7];
-      for (let i=0; i<inducedErrorBlocks.length; i++) {
-        let block = blockchain.getBlock(inducedErrorBlocks[i]);
-        block.body = 'induced chain error';
-      }
-      expect(blockchain.validateChain()).toBe(false);
+      return promise.then(() => {
+        done();
+      });
     });
   });
-  
+
+  afterEach((done) => {
+    blockchain.reset().then(() => {
+      blockchain.close().then(() => done());
+    });
+  });
+
+  it('should validate a correct Blockchain', (done) => {
+    blockchain.validateChain().then(rc => {
+      blockchain.outputChain();
+      expect(rc).toBe(true);
+      done();
+    });
+  });
+
+  it('should raise errors in manipulated chain', (done) => {
+    const inducedErrorBlocks = [2, 4, 7];
+    const inducedPromises = [];
+    for (let i = 0; i < inducedErrorBlocks.length; i++) {
+      let idx = inducedErrorBlocks[i];
+      inducedPromises[i] = blockchain.getBlock(idx).then(block => {
+        block.body = 'induced chain error';
+        return blockchain.updateBlock(idx, block);
+      });
+    }
+    Promise.all(inducedPromises).then(() => {
+      blockchain.validateChain().then(rc => {
+        expect(rc).toBe(false);
+        done();
+      })
+    });
+  });
+});
